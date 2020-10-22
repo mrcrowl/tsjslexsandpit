a0eb05cdcb35f1c6927255c5552dce3a3f03dcfa
mrcrowl <mrcrowl@gmail.com>
2020-10-23 00:05:38 +1300

    Create no-async-promise-executor.ts

---
+++ no-async-promise-executor.ts
@@ -0,0 +1,27 @@
+// bad code
+const foo = new Promise(async (resolve, reject) => {
+  readFile('foo.txt', function(err, result) {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
+
+const result = new Promise(async (resolve, reject) => {
+  resolve(await foo);
+});
+
+// good code
+const foo = new Promise((resolve, reject) => {
+  readFile('foo.txt', function(err, result) {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(result);
+    }
+  });
+});
+
+const result = Promise.resolve(foo);
